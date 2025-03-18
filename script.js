3523ff4d3c3d40fdf4362bf83266e71d7d8df4a7
desc18 <songeitachicham@gmail.com>
2025-03-18 21:01:36 +0600

    Js file
    My website file.

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Website has been loaded!");
+
+    // Show a welcome message to the user
+    alert("Welcome to the DESC Foundation website! We are working together for a better environment.");
+});
